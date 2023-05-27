36ae177388c979c6851ab12d4940ef0171da0f0a
dkmityuk <71889565+dkmityuk@users.noreply.github.com>
2023-05-28 00:54:13 +0300

    [Feat]
    Create base acricetcure

---
+++ FastTouchGame/SourceCode/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FastTouchGame
+//
+//  Created by Dmytro Kmytiuk on 27.05.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    var window: UIWindow?
+
+}
+
